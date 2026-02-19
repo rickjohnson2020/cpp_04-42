ff33f08a844029e75dd58712732d511b328fcdc4
yano <eriksontokyo2020@gmail.com>
2026-02-19 14:53:23 +0000

    add WrongAnimal/Cat class and test for them

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "./Animal.hpp"
+#include "./WrongAnimal.hpp"
 #include "./Dog.hpp"
 #include "./Cat.hpp"
+#include "./WrongCat.hpp"
 
 
 int main() {
@@ -8,15 +10,33 @@ int main() {
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+	std::cout << "Dog type: " << j->getType() << " " << std::endl;
+	std::cout << "Cat type: " << i->getType() << " " << std::endl;
+
+	std::cout << "Cat makes sound: ";
 	i->makeSound();
+
+	std::cout << "Dog makes sound: ";
 	j->makeSound();
+
+	std::cout << "Animal makes sound: ";
 	meta->makeSound();
+
+	std::cout << "=============================" << std::endl;
+
+	const WrongAnimal* wrongMeta = new WrongAnimal();
+	const WrongAnimal* k = new WrongCat();
+
+	std::cout << "WrongCat type: " << k->getType() << " " << std::endl;
+	std::cout << "WrongCat makes sound: ";
+	k->makeSound();
 
 	delete meta;
 	delete j;
 	delete i;
+
+	delete wrongMeta;
+	delete k;
 
 	return 0;
 }

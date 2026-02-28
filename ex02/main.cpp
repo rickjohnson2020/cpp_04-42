454f8b7a290f424aba499261bd274a38494e5312
Riku Yano <riyano@student.42london.com>
2026-02-28 13:51:13 +0000

    improve get/setIdea() and test for them, delete unnecessary code

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -11,22 +11,35 @@ int main() {
 	const Animal* c1 = new Cat();
 	Dog* d2 = new Dog();
 	Dog* d3 = new Dog();
+	std::cout << std::endl;
 
 	std::cout << "Dog type: " << d1->getType() << " " << std::endl;
 	std::cout << "Cat type: " << c1->getType() << " " << std::endl;
+	std::cout << std::endl;
 
 	std::cout << "Cat makes sound: ";
 	c1->makeSound();
 
 	std::cout << "Dog makes sound: ";
 	d1->makeSound();
+	std::cout << std::endl;
 
-	d2->getBrain()->setIdea(0, "I want to be a cat!");
+	if (!d2->getBrain()->setIdea(0, "I want to be a cat!"))
+		std::cout << "Idex out of range" << std::endl;
+	else
+		std::cout << "Idea set. Index: 0" << std::endl;
+	std::cout << std::endl;
+
 	*d3 = *d2;
-	d3->getBrain()->setIdea(0, "I don't want to be a cat!");
+	if (!d3->getBrain()->setIdea(0, "I don't want to be a cat!"))
+		std::cout << "Index out of range" << std::endl;
+	else
+		std::cout << "Idea set. Index: 0" << std::endl;
 
-	std::cout << "d2's idea: " << d2->getBrain()->getIdea(0) << std::endl;
-	std::cout << "d3's idea: " << d3->getBrain()->getIdea(0) << std::endl;
+	std::string idea;
+	std::cout << "d2's idea[0]: " << (d2->getBrain()->getIdea(0, idea) ? idea : "Index out of range") << std::endl;
+	std::cout << "d3's idea[0]: " << (d3->getBrain()->getIdea(0, idea) ? idea : "Index out of range") << std::endl;
+	std::cout << std::endl;
 
 	delete d1;
 	delete d2;
